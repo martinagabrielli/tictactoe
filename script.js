acc7bf5c3eec7012ad41cb07d51a24b0aaa29127
Martina <martina.gabrielli@yahoo.it>
2019-05-10 13:59:27 +0100

    More functionality added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var turn = false;
-
+var finished = false;
 
 function checkThreeSymbols(symbol, a, b, c){
     if(jQuery('#board li:nth-child('+a+')').hasClass(symbol)
@@ -10,48 +10,65 @@ function checkThreeSymbols(symbol, a, b, c){
     else {
         return false;
     }
-    
 }
 
 function markBoard(){
-    jQuery(this).addClass('mark');
- 
-    if(turn === false){
-        turn = true;
-        jQuery(this).addClass('cross');
-    } else {
-        turn = false;
-        jQuery(this).addClass('circle');
-    }
 
+    if(!finished) {
+
+        if(!jQuery(this).hasClass('mark')){
+            jQuery(this).addClass('mark');
     
-    if(
-        checkThreeSymbols('circle',1,2,3) || 
-        checkThreeSymbols('circle',4,5,6) ||
-        checkThreeSymbols('circle',7,8,9) ||
-        checkThreeSymbols('circle',1,4,7) ||
-        checkThreeSymbols('circle',2,5,8) ||
-        checkThreeSymbols('circle',3,6,9) ||
-        checkThreeSymbols('circle',1,5,9) ||
-        checkThreeSymbols('circle',3,5,7)
-    )
-    {
-        alert('O wins!')
+            if(turn === false){
+                turn = true;
+                jQuery(this).addClass('cross');
+            } else {
+                turn = false;
+                jQuery(this).addClass('circle');
+            }
+        }
+        
+        if(
+            checkThreeSymbols('circle',1,2,3) || 
+            checkThreeSymbols('circle',4,5,6) ||
+            checkThreeSymbols('circle',7,8,9) ||
+            checkThreeSymbols('circle',1,4,7) ||
+            checkThreeSymbols('circle',2,5,8) ||
+            checkThreeSymbols('circle',3,6,9) ||
+            checkThreeSymbols('circle',1,5,9) ||
+            checkThreeSymbols('circle',3,5,7)
+        )
+        {
+            finished = true;
+            jQuery('#board').addClass('circle-wins');
+            jQuery('#button').addClass('active')
+        }
+
+        if(
+            checkThreeSymbols('cross',1,2,3) || 
+            checkThreeSymbols('cross',4,5,6) ||
+            checkThreeSymbols('cross',7,8,9) ||
+            checkThreeSymbols('cross',1,4,7) ||
+            checkThreeSymbols('cross',2,5,8) ||
+            checkThreeSymbols('cross',3,6,9) ||
+            checkThreeSymbols('cross',1,5,9) ||
+            checkThreeSymbols('cross',3,5,7)
+        )
+        {
+            finished = true;
+            jQuery('#board').addClass('cross-wins');
+            jQuery('#button').addClass('active')
+        }
     }
 
-    if(
-        checkThreeSymbols('cross',1,2,3) || 
-        checkThreeSymbols('cross',4,5,6) ||
-        checkThreeSymbols('cross',7,8,9) ||
-        checkThreeSymbols('cross',1,4,7) ||
-        checkThreeSymbols('cross',2,5,8) ||
-        checkThreeSymbols('cross',3,6,9) ||
-        checkThreeSymbols('cross',1,5,9) ||
-        checkThreeSymbols('cross',3,5,7)
-    )
-    {
-        alert('X wins!')
-    }
 }
 
+function reset(){
+    jQuery('#board li').removeClass('mark cross circle');
+    jQuery('#board').removeClass('circle-wins cross-wins');
+    jQuery('#button').removeClass('active');
+    finished = false;
+}
+
+jQuery('#button').click(reset);
 jQuery('#board li').click(markBoard);
